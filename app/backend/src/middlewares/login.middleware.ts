daf63a22d5af834b5e6174813888b48891a00e70
Luis Monte <luismonte.l560@gmail.com>
2022-07-19 04:39:45 -0300

    fix(middleware/login): change validation logic

--- app/backend/src/middlewares/login.middleware.ts
+++ app/backend/src/middlewares/login.middleware.ts
@@ -2,19 +2,19 @@ import { Request, Response, NextFunction } from 'express';
 
 export const validateEmail = (req: Request, res: Response, next: NextFunction) => {
   const { email } = req.body;
-  if (email && typeof email === 'string' && email.includes('@') && email.includes('.com')) {
-    return next();
+  if (!email) {
+    return res.status(400).json({ message: 'All fields must be filled' });
   }
 
-  return res.status(400).json({ message: 'All fields must be filled' });
+  next();
 };
 
 export const validatePassword = (req: Request, res: Response, next: NextFunction) => {
   const { password } = req.body;
 
-  if (password && typeof password !== 'string' && password.length >= 6) {
-    return next();
+  if (!password) {
+    return res.status(400).json({ message: 'All fields must be filled' });
   }
 
-  return res.status(400).json({ message: 'All fields must be filled' });
+  next();
 };
