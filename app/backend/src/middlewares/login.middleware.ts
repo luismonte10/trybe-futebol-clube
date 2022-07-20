ce3fbded6f0565066cb7c4fbae3ea03ab11443c8
Luis Monte <luismonte.l560@gmail.com>
2022-07-20 03:04:09 -0300

    feat(login/middleware): check if email and password are correct

--- app/backend/src/middlewares/login.middleware.ts
+++ app/backend/src/middlewares/login.middleware.ts
@@ -1,19 +1,37 @@
 import { Request, Response, NextFunction } from 'express';
+import * as bcrypt from 'bcryptjs';
+import User from '../database/models/users';
 
-export const validateEmail = (req: Request, res: Response, next: NextFunction) => {
+export const validateEmail = async (req: Request, res: Response, next: NextFunction) => {
   const { email } = req.body;
   if (!email) {
     return res.status(400).json({ message: 'All fields must be filled' });
   }
 
+  const user = await User.findOne({ where: { email } });
+
+  if (!user) {
+    return res.status(401).json({ message: 'Incorrect email or password' });
+  }
+
   next();
 };
 
-export const validatePassword = (req: Request, res: Response, next: NextFunction) => {
-  const { password } = req.body;
+export const validatePassword = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
 
   if (!password) {
     return res.status(400).json({ message: 'All fields must be filled' });
+  }
+
+  const user = await User.findOne({ where: { email } });
+
+  if (user) {
+    const isCorrect = bcrypt.compare(password, user.password);
+
+    if (!isCorrect) {
+      return res.status(401).json({ message: 'Incorrect email or password' });
+    }
   }
 
   next();
