bb96feec9283a19d937d8c89abfec222f8895eb9
Luis Monte <luismonte.l560@gmail.com>
2022-07-20 03:06:44 -0300

    feat(login/controller): implements validateToken

--- app/backend/src/controllers/users.controller.ts
+++ app/backend/src/controllers/users.controller.ts
@@ -5,9 +5,24 @@ import userServices from '../services/users.services';
 const login = async (req: Request, res: Response) => {
   const loginUser: ILoginUser = req.body;
 
-  const token = await userServices(loginUser);
+  const token = await userServices.login(loginUser);
 
   return res.status(200).json({ token });
 };
 
-export default login;
+const validateToken = async (req: Request, res: Response) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) return res.status(401).json({ message: 'Expired or invalid token' });
+
+  const user = await userServices.validateToken(authorization);
+
+  return res.status(200).json({ role: user.role });
+};
+
+const userController = {
+  login,
+  validateToken,
+};
+
+export default userController;
