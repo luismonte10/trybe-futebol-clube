24a2d9f1344f1fd3a1c2b825beb6bb2422b65d6b
Luis Monte <luismonte.l560@gmail.com>
2022-07-21 06:49:37 -0300

    feat(database): implement matches model and migrations

---
+++ app/backend/src/database/models/matches.ts
@@ -0,0 +1,54 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Teams from './teams';
+
+class Matches extends Model {
+  public id!: number;
+  public homeTeam!: number;
+  public homeTeamGoals!: number;
+  public awayTeam!: number;
+  public awayTeamGoals!: number;
+  public inProgress!: boolean;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+Teams.hasMany(Matches, { foreignKey: 'homeTeam', as: 'timeCasa' });
+Teams.hasMany(Matches, { foreignKey: 'awayTeam', as: 'timeFora' });
+
+export default Matches;
