923f4574d94d1cadb92a22c0d2c8139df881a0d2
Luis Monte <luismonte.l560@gmail.com>
2022-09-14 21:55:30 -0300

    feat(interface): leaderboard interface

---
+++ app/backend/src/interfaces/leaderboard.interface.ts
@@ -0,0 +1,20 @@
+import { IMatch } from './matches.interface';
+
+export interface ILeaderboard {
+  name: string,
+  totalPoints: number,
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  goalsBalance: number,
+  efficiency: number
+}
+
+export interface ITeamLeaderboard {
+  id: number,
+  teamName: string,
+  teamHome: IMatch[]
+}
