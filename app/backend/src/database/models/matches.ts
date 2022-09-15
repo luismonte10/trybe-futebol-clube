433213ab0398f44f841e193aed703e147e726df3
Luis Monte <luismonte.l560@gmail.com>
2022-09-14 22:00:22 -0300

    fix(models/marches): bug fix

--- app/backend/src/database/models/matches.ts
+++ app/backend/src/database/models/matches.ts
@@ -48,7 +48,7 @@ Matches.init({
 Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'teamHome' });
 Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'teamAway' });
 
-Teams.hasMany(Matches, { foreignKey: 'homeTeam', as: 'timeCasa' });
-Teams.hasMany(Matches, { foreignKey: 'awayTeam', as: 'timeFora' });
+Teams.hasMany(Matches, { foreignKey: 'homeTeam', as: 'teamHome' });
+Teams.hasMany(Matches, { foreignKey: 'awayTeam', as: 'teamAway' });
 
 export default Matches;
